c3da3d5a403b5d7db6ddff7d1fe8729d6df01416
srgg <srggal@gmail.com>
2014-02-08 16:27:39 +0200

    Update torrent-watch.sh

--- torrent-watch.sh
+++ torrent-watch.sh
@@ -1,18 +1,20 @@
 #!/opt/bin/bash
 
+dir=HTorrents
+
 if [ -z "$TR_TORRENT_NAME" ]
 then
-    dropbox_uploader list HTorrents| grep -iE "^ \[F\] .+\.torrent$" | \
+    dropbox_uploader list $dir| grep -iE "^ \[F\] .+\.torrent$" | \
     sed "s|^\s\[F\]\s[0-9]*\s*||g" | \
     while read item
     do
-        file="HTorrents/$item"
+        file="$dir/$item"
         echo "Start downloading $file"
         dropbox_uploader download "$file" "/mnt/transmission/watch/$item"
         dropbox_uploader delete "$file"
     done
 else
     echo "Donwload job done at `date`" > "/tmp/$TR_TORRENT_NAME.txt"
-    dropbox_uploader upload "/tmp/$TR_TORRENT_NAME.txt" HTorrents
+    dropbox_uploader upload "/tmp/$TR_TORRENT_NAME.txt" $dir
     rm -f "/tmp/$TR_TORRENT_NAME.txt"
 fi
